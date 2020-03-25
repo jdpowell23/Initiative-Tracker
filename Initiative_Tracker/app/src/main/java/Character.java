361ac5bcb63a20190bb36c7a089ae9289bbf316b
Justin Powell <justinpowell@Justins-MacBook-Pro-2.local>
2020-03-25 17:48:05 -0400

    class constructor

--- Initiative_Tracker/app/src/main/java/Character.java
+++ Initiative_Tracker/app/src/main/java/Character.java
@@ -1,6 +1,28 @@
-import java.util.Scanner;
-
 public class Character {
 
+    private String name;
+    private String mod;
+
+    public Character(String Justin, String mod) {
+        this.name = name;
+        this.mod = mod;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCrime() {
+        return mod;
+    }
+
+    public Character() {
+        name = "justin";
+    }
+
+    public static void main(String[] args) {
+        Character myObj = new Character(); // Create an object of class MyClass (This will call the constructor)
+        System.out.println(myObj.name); // Print the value of x
+    }
 
 }
--- Initiative_Tracker/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Initiative_Tracker/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,9 +6,17 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static void main(String[] args) {
+        Character char1 = new Character(justin, 1);
+        Character.getName();
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
